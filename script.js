b63f30d83906e688794af8de95385daaa5e89143
DH <116113625+dh-programs@users.noreply.github.com>
2023-02-23 09:24:12 -0800

    Update grid creation

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-const container = document.querySelector('#container');
+const gridContainer = document.querySelector('#grid-container');
 
 function createGrid(rowSize) {
-    let containerSize = container.offsetWidth;
-    let boxSize = containerSize / rowSize;
+    let gridContainerSize = gridContainer.offsetWidth;
+    let boxSize = Math.round(gridContainerSize / rowSize) - 1;
     let boxSizePx = `${boxSize}px`;
-    for (let i = 0; i < rowSize; i++) {
+    console.log(`containerSize is ${gridContainerSize} and boxSizePx is ${boxSizePx}`)
+    for (let i = 0; i < rowSize * rowSize; i++) {
         //let divBox = `grid${i}Box`;
         let divBox = document.createElement('div');
         divBox.classList.add('div');
-        divBox.style.width = boxSizePx;
+        divBox.className = 'grid';
         divBox.style.height = boxSizePx;
-        divBox.style.backgroundColor = 'white';
-        divBox.style.borderStyle = 'solid';
-        divBox.style.borderColor = 'black';
-        divBox.style.borderWidth = '1px';
-        container.append(divBox);
+        divBox.style.width = boxSizePx;
+        divBox.style.maxWidth = boxSizePx;
+        divBox.style.maxHeight = boxSizePx;
+        gridContainer.append(divBox);
+        console.log(`${divBox.offsetHeight} and ${divBox.offsetWidth}`);
     }
 }
 
-createGrid(16);
+createGrid(12);
