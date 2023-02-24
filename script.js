7c4c846f44a448d20c1e8248953924ffa2244ef8
DH <116113625+dh-programs@users.noreply.github.com>
2023-02-23 19:45:06 -0800

    Create functional grid, create color selection function, create clear grid function, create toggle gride line function

--- script.js
+++ script.js
@@ -19,3 +19,53 @@ function createGrid(rowSize) {
 }
 
 createGrid(16);
+
+const gridBoxes = document.querySelectorAll('.grid-boxes');
+gridBoxes.forEach(gridBox => gridBox.addEventListener('mouseover', gridFill));
+
+const clearButton = document.querySelector('#btn-clear-grid');
+clearButton.addEventListener('click', clearGrid);
+
+const gridLineButton = document.querySelector('#btn-grid-line');
+gridLineButton.addEventListener('click', toggleGridLines);
+
+let fillColor = 'black';
+const blackButton = document.querySelector('#btn-black-white');
+blackButton.addEventListener('click', setFillColorBlack);
+
+const vividsButton = document.querySelector('#btn-vivids');
+vividsButton.addEventListener('click', setFillColorVivids);
+
+const pastelsButton = document.querySelector('#btn-pastels');
+pastelsButton.addEventListener('click', setFillColorPastels);
+
+function gridFill() {
+    this.style.backgroundColor = fillColor;
+}
+
+function clearGrid() {
+    gridBoxes.forEach(gridBox => gridBox.style.backgroundColor = 'white');
+}
+
+function toggleGridLines() {
+    gridBoxes.forEach(gridBox => {
+        if (gridBox.style.borderWidth === '1px') {
+            gridBox.style.borderWidth = '0px';
+        } else {
+            gridBox.style.borderWidth = '1px';
+        }
+    });
+}
+
+function setFillColorBlack() {
+    fillColor = 'black';
+}
+
+// choose from array of pastels
+function setFillColorVivids() {
+    fillColor = 'rgb(0, 255, 242)';
+}
+
+function setFillColorPastels() {
+    fillColor = 'rgb(255, 150, 168)';
+}
